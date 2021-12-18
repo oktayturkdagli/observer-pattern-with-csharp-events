4e6750be892f2c44a83de93a6c1d47ccdaa6d9e4
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2021-12-18 20:00:53 +0300

    Initialize

---
+++ Assets/Game/Scripts/Wall/TriggerArea.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerArea : MonoBehaviour
+{
+    public int id;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameEvents.current.DoorwayTriggerEnter(id);
+        Debug.Log("Door Count: " + GameEvents.current.GetOnRequestListOfDoors().Count);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GameEvents.current.DoorwayTriggerExit(id);
+    }
+}
